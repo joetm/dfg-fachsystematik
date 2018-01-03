6ea5bf60eca2cf875ec23ec98ab72595687d4168
Jonas <jonaso@web.de>
2018-01-03 11:43:04 +0100

    update documentation; ghpages

--- createDocumentation.sh
+++ createDocumentation.sh
@@ -5,3 +5,5 @@
 
 # uses config file
 java -jar ../IKON/tools/WIDOCO/jar/widoco-1.4.1-jar-with-dependencies.jar -ontFile ./dfg-fachsystematik.owl -outFolder ./docs -getOntologyMetadata -oops -rewriteAll -confFile ./widocoConfigFile -includeImportedOntologies -htaccess -webVowl -licensius
+
+cp ./README.md ./docs/
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+DFG Fachsystematik
+==================
+
+Struktur der Fachkollegien und Fächer
+
+Quelle: http://www.dfg.de/dfg_profil/gremien/fachkollegien/faecher/
